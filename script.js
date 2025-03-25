d59b8102ea03b01f8cd8e43827c7ecbb22006a2c
Ruella-De-Vil <159031808+Ruella-De-Vil@users.noreply.github.com>
2025-03-25 10:02:54 +0200

    Gradebook App solved

--- script.js
+++ script.js
@@ -46,4 +46,79 @@ function pyramid(char, rows, boolean){
   }
   
   pyramid("o", 4, false);
-  pyramid("p", 5, true)
+  pyramid("p", 5, true);
+
+
+
+ // Build a Gradebook App
+
+/*User Stories:
+
+You should have a function named getAverage that takes an array of test scores as a parameter and returns the average score.
+You should have a function named getGrade that takes a student score as a parameter and returns a string representing a letter grade based on the score. Here are the scores and their corresponding letter grades:
+
+Score Range	Grade
+100	"A+"
+90 - 99	"A"
+80 - 89	"B"
+70 - 79	"C"
+60 - 69	"D"
+0 - 59	"F"
+You should have a function named hasPassingGrade that takes a score as a parameter and returns either true or false depending on if the score corresponds to a passing grade.
+The hasPassingGrade function should use the getGrade function to get the letter grade, and use it to determine if the grade is passing. A passing grade is anything different from "F".
+You should have a function named studentMsg that takes an array of scores and a student score as the parameters. The function should return a string with the format:
+
+"Class average: average-goes-here. Your grade: grade-goes-here. You passed the course.", if the student passed the course.
+"Class average: average-goes-here. Your grade: grade-goes-here. You failed the course.", if the student failed the course.
+Replace average-goes-here with the average of total scores and grade-goes-here with the student's grade. Use getAverage to get the average score and getGrade to get the student's grade.*/
+
+function getAverage(testScoresArr){
+
+    let sumOfScores = 0;
+    for (let testScore of testScoresArr){
+      sumOfScores += testScore;
+    }
+  
+    let numOfScores = testScoresArr.length;
+  
+    let scoreAverage = sumOfScores/numOfScores;
+  
+    return scoreAverage;
+  }
+  
+  let scoresArr = [80,55,75,89,65];
+  console.log(getAverage(scoresArr));
+  
+  function getGrade(studentScore){
+    if(studentScore === 100) return "A+";
+    if(studentScore >= 90) return "A";
+    if(studentScore >= 80) return "B";
+    if(studentScore >= 70) return "C";
+    if(studentScore >= 60) return "D";
+    if(studentScore >= 0) return "F";
+  }
+  
+  console.log(getGrade(getAverage(scoresArr)));
+  
+  const hasPassingGrade = (score) => {
+    return getGrade(score) !== "F";
+  }
+  
+  console.log(hasPassingGrade(80))
+  
+  function studentMsg(scoresArray, studentScore){
+
+    let classAverage = getAverage(scoresArray);
+    let grade = getGrade(studentScore);
+  
+    if(hasPassingGrade(studentScore)){
+      return `Class average: ${classAverage}. Your grade: ${grade}. You passed the course.` ;
+    } else {
+      return `Class average: ${classAverage}. Your grade: ${grade}. You failed the course.`;
+    }
+  }
+  
+  console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 40))
+  
+  
+  
