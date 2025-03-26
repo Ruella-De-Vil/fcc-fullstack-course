2b38bcf3f3f45a42e594c3f796a87afe972ec200
Ruella-De-Vil <159031808+Ruella-De-Vil@users.noreply.github.com>
2025-03-26 21:02:26 +0200

    library manager fCC solution from workshop

--- script.js
+++ script.js
@@ -241,3 +241,101 @@ const generatePassword = (passwordLength) => {
   const password = generatePassword(8);
   console.log("Generated password:" + password)
   
+
+
+  // HIGHER ORDER FUNCTIONS AND CALLBACKS
+
+
+  /*Build a Library manager ----- WORKSHOP fCC Solution*/
+  /* Array methods (forEach(), reduce(), map(), filter()*/
+
+  const library = [
+  {
+    title: 'Your Next Five Moves: Master the Art of Business Strategy',
+    author: 'Patrick Bet-David and Greg Dinkin',
+    about: 'A book on how to plan ahead',
+    pages: 320,
+  },
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    about: 'A practical book about discarding bad habits and building good ones',
+    pages: 320,
+  },
+  {
+    title: 'Choose Your Enemies Wisely: Business Planning for the Audacious Few',
+    author: 'Patrick Bet-David',
+    about:
+      "A book that emphasizes the importance of identifying and understanding one's adversaries to succeed in the business world",
+    pages: 304,
+  },
+  {
+    title: 'The Embedded Entrepreneur',
+    author: 'Arvid Kahl',
+    about: 'A book focusing on how to build an audience-driven business',
+    pages: 308,
+  },
+  {
+    title: 'How to Be a Coffee Bean: 111 Life-Changing Ways to Create Positive Change',
+    author: 'Jon Gordon',
+    about: 'A book about effective ways to lead a coffee bean lifestyle',
+    pages: 256,
+  },
+  {
+    title: 'The Creative Mindset: Mastering the Six Skills That Empower Innovation',
+    author: 'Jeff DeGraff and Staney DeGraff',
+    about: 'A book on how to develop creativity and  innovation skills',
+    pages: 168,
+  },
+  {
+    title: 'Rich Dad Poor Dad',
+    author: 'Robert Kiyosaki and Sharon Lechter',
+    about: 'A book about financial literacy, financial independence, and building wealth. ',
+    pages: 336,
+  },
+  {
+    title: 'Zero to Sold',
+    author: 'Arvid Kahl',
+    about: 'A book on how to bootstrap a business',
+    pages: 500,
+  },
+];
+
+//returns title, author and pages of all books as a string
+function displayBooks(catalog) {
+    let output = 'Books in the Library:\n';
+  
+    catalog.forEach((book) => {
+      output += `- ${book.title} by ${book.author} (${book.pages} pages)\n`;
+    });
+  
+    return output;
+  }
+  
+  /*Create a getBookSummaries function with a single parameter, accepting an array with book objects.
+The getBookSummaries function should return an array containing all the about property value of each book.*/
+  function getBookSummaries(catalog) {
+    return catalog.map((book) => book.about);
+  }
+  
+  /*Create a getBooksByAuthor function with two parameters - an array with book objects and a string with the author.
+The function must return an array that contains the books by a particular author.*/
+  function getBooksByAuthor(catalog, author) {
+    return catalog.filter((book) => book.author === author);
+  }
+  
+  /* create a getTotalPages function with a single parameter, accepting an array with book objects. 
+  The function should return the total number of pages in the books from the array passed to the function.*/
+  function getTotalPages(catalog) {
+    return catalog.reduce((acc, book) => acc + book.pages, 0);
+  }
+
+  const libraryBooks = displayBooks(library);
+  const bookSummaries = getBookSummaries(library);
+  const booksByArvidKahl = getBooksByAuthor(library, "Arvid Kahl");
+  const totalPagesOfBooksInLibrary = getTotalPages(library);
+
+  console.log(libraryBooks);
+  console.log(bookSummaries);
+  console.log(booksByArvidKahl);
+  console.log(totalPagesOfBooksInLibrary);
